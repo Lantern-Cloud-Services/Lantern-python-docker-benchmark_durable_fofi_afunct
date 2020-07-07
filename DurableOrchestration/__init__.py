7c8b03c226620c2385530a63c47e151d1a9a19a8
jdvcDev <jason.cook@microsoft.com>
2020-07-07 20:43:54 +0000

    iterating

--- DurableOrchestration/__init__.py
+++ DurableOrchestration/__init__.py
@@ -8,28 +8,27 @@ import azure.durable_functions as df
 def orchestrator_function(context: df.DurableOrchestrationContext):
     start_time = time.time()
 
+    parallel_tasks = []
+    
     params = context.get_input()
     base = int(params["base"])
     exp = int(params["exp"])
 
     logging.warning(f"Orchestrator Triggered with params - base: " + base.__str__() + ", exp: " + exp.__str__())
-    results = []
+    #results = []
     for x in range(2, exp+1):        
         data = {
             "base": base,
             "exp": x
-        }
-        r = yield context.call_activity("DurableActivity", data)
-        results.append(r)
-    
-    # this summation could also be done via an additional activity function
-    total = 0
-    for val in results:
-        total += val
-    
+        }        
+        parallel_tasks.append(context.call_activity("DurableActivity", data))
+
+    results = yield context.task_all(parallel_tasks)
+    total = sum(results)
+
     end_time = time.time()
     total_time = end_time-start_time
-    final_result = f"With the input values base: {base}, exp: {exp} we calculated a total result of {total} in {total_time} seconds"
+    final_result = f"With the input values base: {base}, exp: {exp} we calculated a total result of {total} in {total_time} seconds using FOFI."
 
     return final_result
 
